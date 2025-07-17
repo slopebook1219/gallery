c082ed2c3ff9f33daaf6c07fceb641303d838768
坂本大幹 <saka.ttt.1219@gmail.com>
2025-07-17 16:27:20 +0900

    スライド作成

---
+++ app/utils/getMvImages.tsx
@@ -0,0 +1,9 @@
+export default function getMvImages(): string[] {
+  const MvImages: string[] = [];
+  //1枚目
+  MvImages.push("/MvImages/5A741594-1B2E-43AE-8FC7-919D9B23C008_1_105_c.jpeg");
+  MvImages.push("/MvImages/952403D0-BEE5-456C-B2B9-BF57573741E8_1_105_c.jpeg");
+  MvImages.push("/MvImages/DSC06245.JPG");
+  MvImages.push("/MvImages/C85EF57F-531E-4C28-8064-4DD883514D9C.jpg");
+  return MvImages;
+}

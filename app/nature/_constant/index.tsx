772962cd452c9fa25fb273b803d3bad408c3888c
坂本大幹 <saka.ttt.1219@gmail.com>
2025-09-30 16:59:21 +0900

    写真を追加

--- app/nature/_constant/index.tsx
+++ app/nature/_constant/index.tsx
@@ -7,11 +7,34 @@ export type ImageType = {
   comment?: string;
 };
 export const Images = [
-  {
+
+    {
     id: 1,
-    src: "/nature/DSC07719.JPG",
-    alt: "100年記念館天井",
+    src: "/nature/IMG_0673.JPG",
+    alt: "#",
     camera: "NATURA CLASSICA",
     film: "kodak Color Plus",
   },
+    {
+    id: 2,
+    src: "/nature/IMG_0795.JPG",
+    alt: "#",
+    camera: "NATURA CLASSICA",
+    film: "kodak Color Plus",
+  },
+    {
+    id: 3,
+    src: "/nature/1F34DE20-C083-4505-BCD0-F9D5AF3569D3.JPG",
+    alt: "#",
+    camera: "NATURA CLASSICA",
+    film: "kodak Color Plus",
+  },
+  {
+    id: 4,
+    src: "/nature/FF106352-6B10-474D-9D1D-54F41F1C9C07.JPG",
+    alt: "#",
+    camera: "NATURA CLASSICA",
+    film: "kodak Color Plus",
+  },
+   
 ];

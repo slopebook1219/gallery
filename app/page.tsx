452b12932bf3ddcfa27d0646f8cc8c2f6efbc21d
坂本大幹 <saka.ttt.1219@gmail.com>
2025-10-04 01:36:05 +0900

    Homeのメタ情報を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import MvSlider from "./component/_item/MvSliders/ index";
 import getMvImages from "./utils/getMvImages";
-
+export const metadata = {
+  title: "sakamoto taiki",
+  description: "坂本大幹のサイト",
+}
 export default function Home() {
   const mvImages = getMvImages();
 

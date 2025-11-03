a7104e51a341a2d072640efb5418e782b3f8f144
坂本大幹 <saka.ttt.1219@gmail.com>
2025-11-03 11:06:23 +0900

    メタ情報の修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import MvSlider from "./component/_item/MvSliders/ index";
 import getMvImages from "./utils/getMvImages";
 export const metadata = {
-  title: "sakamoto taiki",
-  description: "坂本大幹のサイト",
-}
+  title: "坂本大幹",
+  description:
+    "坂本大幹のサイトです。カメラで撮影した様々な日常のシーンを掲載しています。ぜひご覧ください。",
+};
 export default function Home() {
   const mvImages = getMvImages();
 
